810cd755b8a81abef203d9ba91d98a93dd0bf7d6
Vitalii Baz <baz.vitalii@gmail.com>
2015-08-28 22:09:05 +0300

    #7 converted unit test for Travel

--- tests/test_travel.py
+++ tests/test_travel.py
@@ -1,8 +1,15 @@
 from sys import path
 path.append('../src')
 
+import unittest
 from travel import Travel
 
-r = Travel('Kyiv', 'Oslo')
-assert r.from_city == 'Kyiv'
-assert r.to_city == 'Oslo'
+class TestTravel(unittest.TestCase):
+	def test_constructor(self):
+		travel = Travel('Kyiv', 'Oslo')
+
+		self.assertEqual('Kyiv', travel.from_city)
+		self.assertEqual('Oslo', travel.to_city)
+
+if __name__ == '__main__':
+	unittest.main()
