8a65189e200e2af24e753bcb2d40c4ae293d10ac
Vitalii Baz <baz.vitalii@gmail.com>
2015-08-28 22:46:06 +0300

    #7 converted unit test for TrainRoute

--- tests/test_train_route.py
+++ tests/test_train_route.py
@@ -1,12 +1,19 @@
 from sys import path
 path.append('../src')
 
+import unittest
 from train_route import TrainRoute
 
-instance = TrainRoute("Vil'no", '23:19', '09:29', '49', "VIL'NO PARMA", '5/09-3/10/2015 DAILY (EXCEPT 6,7,8,9,10,11/09/2015)')
-assert instance.departure_station == "Vil'no"
-assert instance.departure_time == '23:19'
-assert instance.arrival_time == '09:29'
-assert instance.number_train == '49'
-assert instance.route == "Vil'no Parma"
-assert instance.periodicity == '5/09-3/10/2015 daily (except 6,7,8,9,10,11/09/2015)'
+class TestTrainRoute(unittest.TestCase):
+	def test_constructor(self):
+		train_route = TrainRoute("Vil'no", '23:19', '09:29', '49', "VIL'NO PARMA", '5/09-3/10/2015 DAILY (EXCEPT 6,7,8,9,10,11/09/2015)')
+
+		self.assertEqual("Vil'no", train_route.departure_station)
+		self.assertEqual('23:19', train_route.departure_time)
+		self.assertEqual('09:29', train_route.arrival_time)
+		self.assertEqual('49', train_route.number_train)
+		self.assertEqual("Vil'no Parma", train_route.route)
+		self.assertEqual('5/09-3/10/2015 daily (except 6,7,8,9,10,11/09/2015)', train_route.periodicity)
+
+if __name__ == '__main__':
+	unittest.main()
