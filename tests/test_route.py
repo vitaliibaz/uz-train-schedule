685f3d0b068cfde041b2c0be2ddf903dd00a9463
Vitalii Baz <baz.vitalii@gmail.com>
2015-08-30 16:41:30 +0300

    #10 fixed test: prepare two TrainRoute objects( first_train and second_train), update asserts

--- tests/test_route.py
+++ tests/test_route.py
@@ -3,14 +3,19 @@ path.append('..')
 
 import unittest
 from src.route import Route
+from src.train_route import TrainRoute
 
 class TestRoute(unittest.TestCase):
 	def test_constructor(self):
-		route = Route(14121, 777, 14)
+		first_train = TrainRoute('Краків (Польща)', '20:52', '07:15', '3310', 'ВРОЦЛАВ ЛЬВІВ', '14/09-16/10/2015 ЩОДЕННО')
+		second_train = TrainRoute('ЛЬВІВ', '09:40', '16:02', '74', 'ЛЬВІВ МОСКВА', 'З 29/03/2015 ЩОДЕННО (КРІМ 2/09/2015)')
+		total_time_route = '19'
 
-		self.assertEqual(14121, route.first_train)
-		self.assertEqual(777, route.second_train)
-		self.assertEqual(14, route.total_time)
+		route = Route(first_train, second_train, total_time_route)
+
+		self.assertEqual(first_train, route.first_train)
+		self.assertEqual(second_train, route.second_train)
+		self.assertEqual(total_time_route, route.total_time)
 
 if __name__ == '__main__':
 	unittest.main()
