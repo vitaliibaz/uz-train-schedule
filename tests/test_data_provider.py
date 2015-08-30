e58aff9d94a6280ca748833bcb2c128dd4e496d0
Vitalii Baz <baz.vitalii@gmail.com>
2015-08-30 20:00:21 +0300

    close #12: create test for load_stations

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -3,12 +3,19 @@ path.append('../src')
 
 import unittest
 
-from data_provider import load_routes
+from data_provider import load_routes, load_stations
 from station import Station
 from route import Route
 
 
 class TestDataProvider(unittest.TestCase):
+	def test_load_stations(self):
+		stations = load_stations()
+
+		self.assertEqual(2088, len(stations))
+		for station in stations:
+			self.assertEqual(Station, type(station))
+
 	def test_load_routes(self):
 		first_station = Station('Краків (Польща)', '2157,2305')
 		second_station = Station('Вінниця (Україна)', '22200')
