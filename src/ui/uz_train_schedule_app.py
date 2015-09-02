2d9569d80b2ee32ebe5410b60fd50fdde6d9721f
Vitalii Baz <baz.vitalii@gmail.com>
2015-09-02 20:50:08 +0300

    #14 extract window code into separate files

--- src/ui/uz_train_schedule_app.py
+++ src/ui/uz_train_schedule_app.py
@@ -3,13 +3,15 @@ kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
+
+from main_window import MainWindow
 
 
 class UZTrainScheduleApp(App):
     def build(self):
-        return FloatLayout()
+        return MainWindow()
 
 if __name__ == "__main__":
-	Builder.load_file("uz_train_schedule.kv")
+	Builder.load_file("main_window.kv")
+
 	UZTrainScheduleApp().run()
