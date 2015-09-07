4ee9d7c4961837126e44270d85de12b87ee74bb9
Vitalii Baz <baz.vitalii@gmail.com>
2015-09-07 23:36:53 +0300

    close #19 show real stations

--- src/ui/select_station_window.py
+++ src/ui/select_station_window.py
@@ -1,16 +1,22 @@
+from sys import path
+path.append('..')
+
 from kivy.uix.screenmanager import Screen
 from kivy.adapters.dictadapter import DictAdapter
 from kivy.uix.listview import ListView
 from kivy.uix.textinput import TextInput
+
+from data_provider import load_stations
 
 
 class SelectStationWindow(Screen):
 	def __init__(self, **kwargs):
 		super(SelectStationWindow, self).__init__(**kwargs)
 
+		stations = load_stations()
 		data = {}
-		for i in range(100):
-			data[str(i)] = {'text': 'Station ' + str(i), 'is_selected': False}
+		for number in range(len(stations)):
+			data[str(number)] = stations[number]
 
 
 		dict_adapter = DictAdapter(data=data,
@@ -23,11 +29,8 @@ class SelectStationWindow(Screen):
 		self.add_widget(filter_station)
 		self.add_widget(list_view)
 
-
-
 	def station_converter(self, row_index, station):
-		converted = {'text': station['text'],
-					 'is_selected': station['is_selected'],
+		converted = {'text': station.name,
 					 'size_hint_y': None,
 					 'height': 120,
 					 'manager': self.manager}
