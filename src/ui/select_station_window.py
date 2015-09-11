af8b7b12cfe4c56efc1a8162af0b2a4dd7bb6bf2
Vitalii Baz <baz.vitalii@gmail.com>
2015-09-11 23:34:34 +0300

    #24 extract function prepare_stations_dict_adapter

--- src/ui/select_station_window.py
+++ src/ui/select_station_window.py
@@ -11,14 +11,7 @@ class SelectStationWindow(Screen):
 		super(SelectStationWindow, self).__init__(**kwargs)
 
 		stations = load_stations()
-		data = {}
-		for number in range(len(stations)):
-			data[str(number)] = stations[number]
-
-
-		dict_adapter = DictAdapter(data=data,
-								   args_converter=self.station_converter,
-								   template='StationTemplate')
+		dict_adapter = self.prepare_stations_dict_adapter(stations, '')
 
 		filter_station = TextInput(pos_hint={"top":1}, hint_text='Start typing station name', multiline=False, size_hint_y=None, font_size=50)
 		list_view = ListView(pos_hint={"top":0.85}, adapter=dict_adapter)
@@ -32,3 +25,16 @@ class SelectStationWindow(Screen):
 					 'height': 120,
 					 'manager': self.manager}
 		return converted
+
+	def prepare_stations_dict_adapter(self, stations, filter_text):
+		data = {}
+		for number in range(len(stations)):
+			station = stations[number]
+			if filter_text in station.name:
+				data[str(number)] = station
+
+
+		dict_adapter = DictAdapter(data=data,
+								   args_converter=self.station_converter,
+								   template='StationTemplate')
+		return dict_adapter
