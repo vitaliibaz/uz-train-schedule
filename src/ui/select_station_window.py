84aaab20fac8aff59c1022bb968ef17e65eb1cac
Vitalii Baz <baz.vitalii@gmail.com>
2015-09-26 19:50:16 +0300

    close #40 shorten names in list of station

--- src/ui/select_station_window.py
+++ src/ui/select_station_window.py
@@ -27,7 +27,7 @@ class SelectStationWindow(Screen):
 		self.add_widget(self.list_view)
 
 	def station_converter(self, row_index, station):
-		converted = {'text': station.name,
+		converted = {'text': station.name.replace('Російська Федерація', 'РФ'),
 					 'size_hint_y': None,
 					 'height': 120,
 					 'station': station,
