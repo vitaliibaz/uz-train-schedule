a020f02dbc39a2c454fddeef2e1a299ff707067c
Vitalii Baz <baz.vitalii@gmail.com>
2015-09-28 11:15:11 +0300

    close #39 delete Travel class

--- src/travel.py
+++
@@ -1,6 +0,0 @@
-class Travel(object):
-	"""Enter the main parameters of the trip departure station and destination station"""
-	def __init__(self, from_city, to_city):
-		super(Travel, self).__init__()
-		self.from_city = from_city
-		self.to_city = to_city
