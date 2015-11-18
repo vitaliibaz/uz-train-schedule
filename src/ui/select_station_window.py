b18dff5165b8ba1b70c4cd2db28d505ed036ae10
Vitalii Baz <baz.vitalii@gmail.com>
2015-11-18 23:44:44 +0200

    #87 resized buttons and added shorten

--- src/ui/select_station_window.py
+++ src/ui/select_station_window.py
@@ -20,7 +20,7 @@ class SelectStationWindow(Screen):
     def prepare(self, args):
 
         self.clear_widgets()
-        filter_station = TextInput(pos_hint={"top":1}, hint_text='Почніть вводити назву станції', multiline=True, size_hint_y=None, height='60sp', font_size='20sp')
+        filter_station = TextInput(pos_hint={"top":1}, hint_text='Почніть вводити назву станції', multiline=True, size_hint_y=0.15, font_size='20sp')
         filter_station.bind(text=self.on_filter_changed)
 
         dict_adapter = self.prepare_stations_dict_adapter(self.stations, '')
