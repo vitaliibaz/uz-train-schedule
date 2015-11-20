e6f34847fd67a4523b289960e801e58d42e2214b
Vitalii Baz <baz.vitalii@gmail.com>
2015-11-20 11:43:10 +0200

    #87 resized textinput filter station

--- src/ui/select_station_window.py
+++ src/ui/select_station_window.py
@@ -20,11 +20,11 @@ class SelectStationWindow(Screen):
     def prepare(self, args):
 
         self.clear_widgets()
-        filter_station = TextInput(pos_hint={"top":1}, hint_text='Почніть вводити назву станції', multiline=True, size_hint_y=0.15, font_size='20sp')
+        filter_station = TextInput(pos_hint={"top":1}, hint_text='Почніть вводити назву станції', size_hint_y=0.07, font_size='20sp')
         filter_station.bind(text=self.on_filter_changed)
 
         dict_adapter = self.prepare_stations_dict_adapter(self.stations, '')
-        self.list_view = ListView(pos_hint={"top":0.85}, adapter=dict_adapter)
+        self.list_view = ListView(pos_hint={"top":0.93}, adapter=dict_adapter)
 
         self.add_widget(filter_station)
         self.add_widget(self.list_view)
@@ -41,7 +41,7 @@ class SelectStationWindow(Screen):
     def on_filter_changed(self, filter_station, filter_text):
         dict_adapter = self.prepare_stations_dict_adapter(self.stations, filter_text.decode('utf-8'))
         self.remove_widget(self.list_view)
-        self.list_view = ListView(pos_hint={"top":0.85}, adapter=dict_adapter)
+        self.list_view = ListView(pos_hint={"top":0.93}, adapter=dict_adapter)
         self.add_widget(self.list_view)
 
     def prepare_stations_dict_adapter(self, stations, filter_text):
